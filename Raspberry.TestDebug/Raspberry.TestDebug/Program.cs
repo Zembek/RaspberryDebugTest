aeea316b9ffc8b6a24e86ac25eed8b6f998c9e7a
Łukasz Ziemblicki <lzz1990@tlen.pl>
2020-12-17 20:13:09 +0100

    Initial commit

---
+++ Raspberry.TestDebug/Raspberry.TestDebug/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+namespace Raspberry.TestDebug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("App is up");
+#if DEBUG
+            while (true)
+            {
+                Console.WriteLine("Waiting for VS");
+                Thread.Sleep(1000);
+            }
+#endif
+            var tmp = 0;
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
