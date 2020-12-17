2b8888328252e468efa8b50a391c92e61363b541
Łukasz Ziemblicki <lzz1990@tlen.pl>
2020-12-17 21:20:42 +0100

    fix debug file

--- debug.sh
+++ debug.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git pull
 dotnet build Raspberry.TestDebug/Raspberry.TestDebug/Raspberry.TestDebug.csproj -c Debug
-dotnet Raspberry.TestDebug/Raspberry.TestDebug/bin/Debug/netcoreapp3.1/Raspberry.TestDebug.dl
+dotnet Raspberry.TestDebug/Raspberry.TestDebug/bin/Debug/netcoreapp3.1/Raspberry.TestDebug.dll
