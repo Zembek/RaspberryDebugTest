f6486cb7b9a90e26f13e8da1a8f6132aa3bf6c97
Łukasz Ziemblicki <lzz1990@tlen.pl>
2020-12-17 20:35:11 +0100

    use relays

--- Raspberry.TestDebug/Raspberry.TestDebug/Program.cs
+++ Raspberry.TestDebug/Raspberry.TestDebug/Program.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Device.Gpio;
 using System.Threading;
 
 namespace Raspberry.TestDebug
 {
     class Program
     {
+        private static List<int> _pinouts = new List<int> { 26, 19, 13, 6, 5, 11 };
+
         static void Main(string[] args)
         {
             Console.WriteLine("App is up");
@@ -12,9 +16,27 @@ namespace Raspberry.TestDebug
             Console.WriteLine("Press any key to continue");
             Console.ReadKey();
 #endif
-            Console.WriteLine("Please write something");
-            string test = Console.ReadLine();
-            Console.WriteLine("Hello World!");
+            GpioController controller = new GpioController();
+            Console.WriteLine("Initialize piouts");
+            foreach (var pinNo in _pinouts)
+                controller.OpenPin(pinNo, PinMode.Output);
+            Console.WriteLine("END Initialize piouts");
+
+            while (true)
+            {
+                foreach (var pinNo in _pinouts)
+                {
+                    Console.WriteLine($"Set pin: {pinNo} value: Low");
+                    controller.Write(pinNo, PinValue.Low);
+                    Console.WriteLine($"Sleep 1500ms");
+                    Thread.Sleep(1500);
+                    Console.WriteLine($"Set pin: {pinNo} value: High");
+                    controller.Write(pinNo, PinValue.High);
+                    Console.WriteLine($"Sleep 1000ms");
+                    Thread.Sleep(1000);
+                }
+
+            }
         }
     }
 }
